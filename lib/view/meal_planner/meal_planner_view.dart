7d2b5538460bb9fd47db0e7fc5feeab766c362cd
Matt <mathiasseyram11@gmail.com>
2026-07-03 21:05:04 +0000

    meal planner updates

--- lib/view/meal_planner/meal_planner_view.dart
+++ lib/view/meal_planner/meal_planner_view.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:healthsnap/common/color_extension.dart';
 
@@ -11,6 +12,8 @@ class MealPlannerView extends StatefulWidget {
 class _MealPlannerViewState extends State<MealPlannerView> {
   @override
   Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: TColor.white,
@@ -47,9 +50,7 @@ class _MealPlannerViewState extends State<MealPlannerView> {
         ),
         actions: [
           InkWell(
-            onTap: () {
-              //Navigator.pop(context);
-            },
+            onTap: () {},
             child: Container(
               margin: const EdgeInsets.all(8),
               height: 40,
@@ -70,7 +71,249 @@ class _MealPlannerViewState extends State<MealPlannerView> {
         ],
       ),
       backgroundColor: TColor.white,
-      body: SingleChildScrollView(child: Column(children: [])),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: media.width * 0.1),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Workout Progress",
+                  style: TextStyle(
+                    color: TColor.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Container(
+                  height: 30,
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: TColor.primaryG),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: DropdownButton(
+                    items: ["Weekly", "Monthly"]
+                        .map(
+                          (name) => DropdownMenuItem(
+                            value: name,
+                            child: Text(
+                              name,
+                              style: TextStyle(
+                                color: TColor.gray,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                    onChanged: (value) {},
+                    icon: Icon(Icons.arrow_drop_down, color: TColor.white),
+                    hint: Text(
+                      "Weekly",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: TColor.white, fontSize: 12),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: media.width * 0.1),
+            Container(
+              padding: const EdgeInsets.only(left: 15),
+              height: media.width * 0.5,
+              width: double.maxFinite,
+              child: LineChart(
+                LineChartData(
+                  lineTouchData: LineTouchData(
+                    enabled: true,
+                    handleBuiltInTouches: false,
+                    touchCallback:
+                        (FlTouchEvent event, LineTouchResponse? response) {},
+                    mouseCursorResolver:
+                        (FlTouchEvent event, LineTouchResponse? response) {
+                          if (response == null ||
+                              response.lineBarSpots == null) {
+                            return SystemMouseCursors.basic;
+                          }
+                          return SystemMouseCursors.click;
+                        },
+                    getTouchedSpotIndicator:
+                        (LineChartBarData barData, List<int> spotIndexes) {
+                          return spotIndexes.map((index) {
+                            return TouchedSpotIndicatorData(
+                              FlLine(color: Colors.transparent),
+                              FlDotData(
+                                show: true,
+                                getDotPainter:
+                                    (spot, percent, barData, index) =>
+                                        FlDotCirclePainter(
+                                          radius: 3,
+                                          color: TColor.white,
+                                          strokeWidth: 3,
+                                          strokeColor: TColor.secondaryColor1,
+                                        ),
+                              ),
+                            );
+                          }).toList();
+                        },
+                    touchTooltipData: LineTouchTooltipData(
+                      getTooltipColor: (spot) => TColor.secondaryColor1,
+                      tooltipRoundedRadius: 20,
+                      getTooltipItems: (List<LineBarSpot> linearBarsSpot) {
+                        return linearBarsSpot.map((lineBarSpot) {
+                          return LineTooltipItem(
+                            "${lineBarSpot.x.toInt()} mins ago",
+                            const TextStyle(
+                              color: Colors.white,
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          );
+                        }).toList();
+                      },
+                    ),
+                  ),
+                  lineBarsData: lineBarsData1,
+                  minY: -0.5,
+                  maxY: 110,
+                  titlesData: FlTitlesData(
+                    show: true,
+                    leftTitles: AxisTitles(),
+                    topTitles: AxisTitles(),
+                    bottomTitles: AxisTitles(sideTitles: bottomTitles()),
+                    rightTitles: AxisTitles(sideTitles: rightTitles()),
+                  ),
+                  gridData: FlGridData(
+                    show: true,
+                    drawHorizontalLine: true,
+                    horizontalInterval: 25,
+                    drawVerticalLine: false,
+                    getDrawingHorizontalLine: (value) {
+                      return FlLine(
+                        color: TColor.gray.withOpacity(0.15),
+                        strokeWidth: 2,
+                      );
+                    },
+                  ),
+                  borderData: FlBorderData(
+                    show: true,
+                    border: Border.all(color: Colors.transparent),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: media.width * 0.05),
+          ],
+        ),
+      ),
     );
+  }
+
+  List<LineChartBarData> get lineBarsData1 => [lineChartBarData1_1];
+
+  LineChartBarData get lineChartBarData1_1 => LineChartBarData(
+    isCurved: true,
+    gradient: LinearGradient(
+      colors: [
+        TColor.primaryColor2.withOpacity(0.5),
+        TColor.primaryColor1.withOpacity(0.5),
+      ],
+    ),
+    barWidth: 4,
+    isStrokeCapRound: true,
+    dotData: const FlDotData(show: false),
+    belowBarData: BarAreaData(show: false),
+    spots: const [
+      FlSpot(1, 35),
+      FlSpot(2, 70),
+      FlSpot(3, 40),
+      FlSpot(4, 80),
+      FlSpot(5, 25),
+      FlSpot(6, 70),
+      FlSpot(7, 35),
+    ],
+  );
+
+  SideTitles rightTitles() => SideTitles(
+    getTitlesWidget: rightTitleWidgets,
+    showTitles: true,
+    interval: 20,
+    reservedSize: 40,
+  );
+
+  Widget rightTitleWidgets(double value, TitleMeta meta) {
+    String text;
+
+    switch (value.toInt()) {
+      case 0:
+        text = '0%';
+        break;
+      case 20:
+        text = '20%';
+        break;
+      case 40:
+        text = '40%';
+        break;
+      case 60:
+        text = '60%';
+        break;
+      case 80:
+        text = '80%';
+        break;
+      case 100:
+        text = '100%';
+        break;
+      default:
+        return Container();
+    }
+
+    return Text(
+      text,
+      style: TextStyle(color: TColor.gray, fontSize: 12),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  SideTitles bottomTitles() => SideTitles(
+    showTitles: true,
+    reservedSize: 32,
+    interval: 1,
+    getTitlesWidget: bottomTitleWidgets,
+  );
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    var style = TextStyle(color: TColor.gray, fontSize: 16);
+    Widget text;
+
+    switch (value.toInt()) {
+      case 1:
+        text = Text('Sun', style: style);
+        break;
+      case 2:
+        text = Text('Mon', style: style);
+        break;
+      case 3:
+        text = Text('Tue', style: style);
+        break;
+      case 4:
+        text = Text('Wed', style: style);
+        break;
+      case 5:
+        text = Text('Thu', style: style);
+        break;
+      case 6:
+        text = Text('Fri', style: style);
+        break;
+      case 7:
+        text = Text('Sat', style: style);
+        break;
+      default:
+        text = const Text('');
+        break;
+    }
+
+    return SideTitleWidget(axisSide: meta.axisSide, space: 10, child: text);
   }
 }
