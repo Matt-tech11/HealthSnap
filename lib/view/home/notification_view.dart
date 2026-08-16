c1a36d4269feaad9775d24bac593eee5d2e29be7
Matt <mathiasseyram11@gmail.com>
2026-08-16 22:31:19 +0000

    updates

--- lib/view/home/notification_view.dart
+++ lib/view/home/notification_view.dart
@@ -77,29 +77,29 @@ class _NotificationViewState extends State<NotificationView> {
             fontWeight: FontWeight.w500,
           ),
         ),
-        actions: [
-          InkWell(
-            onTap: () {
-              //Navigator.pop(context);
-            },
-            child: Container(
-              margin: const EdgeInsets.all(8),
-              height: 40,
-              width: 40,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: TColor.LightGray,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Image.asset(
-                "assets/img/more_nav.png",
-                width: 13,
-                height: 13,
-                fit: BoxFit.contain,
-              ),
-            ),
-          ),
-        ],
+        // actions: [
+        //   InkWell(
+        //     onTap: () {
+        //       //Navigator.pop(context);
+        //     },
+        //     child: Container(
+        //       margin: const EdgeInsets.all(8),
+        //       height: 40,
+        //       width: 40,
+        //       alignment: Alignment.center,
+        //       decoration: BoxDecoration(
+        //         color: TColor.LightGray,
+        //         borderRadius: BorderRadius.circular(10),
+        //       ),
+        //       child: Image.asset(
+        //         "assets/img/more_nav.png",
+        //         width: 13,
+        //         height: 13,
+        //         fit: BoxFit.contain,
+        //       ),
+        //     ),
+        //   ),
+        // ],
       ),
       backgroundColor: TColor.white,
       body: ListView.separated(
